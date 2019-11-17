ccc79708a2166f4a577098580b5a0a2f146e750b
marita30 <marita.jose.ch@gmail.com>
2019-11-16 18:43:31 -0600

    Add the methodos create, update and delete notes in the file called APP.JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { css } from 'glamor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from './components/Form';
+import Notes from './components/Notes/Notes.js'
+
+
+
+
+class  App extends Components {
+  // Definiendo el estado actual
+  state = { notes: [], filter: 'none' }
+
 }
+
+createNote = async note  => {
+  const notes = [note, ...this.state.notes]
+  const newNotes = this.state.notes
+  this.setState({ notes })
+}
+
+updateNote = async note  => {
+  const uptadedNote = {
+    ...note,
+    status: note.status === 'new' ? 'completed' : "new"
+  }
+  const index = this.state.notes.findIndex(i => i.id == note.id)
+  const notes = [...this.state.notes]
+  notes[index] = updateNote
+  this.setState({ notes })
+}
+
+deleteNote = async note => {
+  const input = { id: note.id }
+  const
+}
+
+
+
+
 
 export default App;
